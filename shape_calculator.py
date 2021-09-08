4a58f51f631cd4cb1ef4ab057a9b8df7a70cbd10
Brian Marquis <rbmarquis.dev@gmail.com>
2021-09-07 19:26:16 -0500

    finish classes

--- shape_calculator.py
+++ shape_calculator.py
@@ -3,29 +3,39 @@ class Rectangle:
         self.width = width
         self.height = height
 
-    def set_width(self):
-        pass
+    def set_width(self, width):
+        self.width = width
 
-    def set_height(self):
-        pass
+    def set_height(self, height):
+        self.height = height
 
     def get_area(self):
-        pass
+        return self.width * self.height
 
     def get_perimeter(self):
-        pass
+        return (self.width * 2) + (self.height * 2)
 
     def get_diagonal(self):
-        pass
+        # diagonal = sqrt of (width squared + height squared)
+        return ((self.width ** 2) + (self.height ** 2)) ** (1/2)
 
     def get_picture(self):
-        pass
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
 
-    def get_amount_inside(self):
-        pass
+        picture = ''
+        for line in range(self.height):
+            picture += "*" * self.width + "\n"
+        
+        return picture
+
+    def get_amount_inside(self, shape):
+        num_widths = self.width // shape.width
+        num_heights = self.height // shape.height
+        return num_widths * num_heights
 
     def __str__(self):
-        return f"Rectangle(width={self.width},height={self.height})"
+        return f"Rectangle(width={self.width}, height={self.height})"
 
 
 
@@ -33,14 +43,15 @@ class Square(Rectangle):
     def __init__(self, side_length):
         super().__init__(side_length, side_length)
 
-    def set_side(self):
-        pass
+    def set_side(self, side_length):
+        self.width = side_length
+        self.height = side_length
 
-    def set_width(self):
-        pass
+    def set_width(self, width):
+        self.set_side(width)
 
-    def set_height(self):
-        pass
+    def set_height(self, height):
+        self.set_side(height)
 
     def __str__(self):
-        return f"Square(side={self.height}"
+        return f"Square(side={self.height})"
